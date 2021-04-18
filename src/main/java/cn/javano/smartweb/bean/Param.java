3e97463ef4a1f67106763b4e57c240ebf2da5242
ssnancr <zhancandong7@126.com>
2021-04-18 19:31:08 +0800

    1.晚上请求参数类

--- src/main/java/cn/javano/smartweb/bean/Param.java
+++ src/main/java/cn/javano/smartweb/bean/Param.java
@@ -1,9 +1,26 @@
 package cn.javano.smartweb.bean;
 
+import java.util.Map;
+
 /**
  * @author: wangjie19558
  * Date:2016/10/30
  * Description:请求参数对象
  */
 public class Param {
+
+    private Map<String, Object> paramMap = null;
+
+    public Param(Map<String, Object> paramMap) {
+        this.paramMap = paramMap;
+    }
+
+    /**
+     * 获取所有字段信息
+     *
+     * @return
+     */
+    public Map<String, Object> getMap() {
+        return paramMap;
+    }
 }
